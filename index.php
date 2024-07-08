960e048d33b9dea3b3e08175999128f8455f72c3
Yagyaraj Majhi <majhiyagya123@gmail.com>
2024-07-08 15:06:03 +0545

    Initial Plugin Commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Plugin Name:       Bloggy Plus
+ * Plugin URI:        https://www.google.com
+ * Description:       This is a Plugin associated with Bloggy Theme. This will help us to add blocks to the theme.
+ * Version:           1.0
+ * Requires at least: 5.9
+ * Requires PHP:      7.2
+ * Author:            Yagyaraj Majhi
+ * Author URI:        https://www.google.com/
+ * Text Domain:       bloggy-plus
+ * Domain Path:       /languages
+ */
+
+if (!function_exists('add_action')) {
+  echo "Do not try this again";
+  exit;
+}
